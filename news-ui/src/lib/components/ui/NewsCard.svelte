0b21bee09b6b1c3cbea9b6049f98c3cc56fe5616
2ulqarnain <zulqernaynh@gmail.com>
2025-11-06 18:32:18 +0500

    changes

--- news-ui/src/lib/components/ui/NewsCard.svelte
+++ news-ui/src/lib/components/ui/NewsCard.svelte
@@ -7,10 +7,13 @@
 
 <a
 	href={newsUrl}
-	class="news-card flex items-center gap-2 overflow-hidden rounded-xl bg-white hover:z-10 hover:scale-105 hover:bg-primary hover:text-white hover:transition-transform"
+	class="news-card group relative flex items-center gap-2 overflow-hidden rounded-xl bg-white hover:z-10 hover:scale-105 hover:bg-primary hover:text-white hover:transition-transform"
 >
 	<img src={imageUrl} alt={title} class="z-0 size-24 min-w-24 bg-primary object-cover" />
 	<p class="headline z-10 line-clamp-2 text-xl">
 		{title}
 	</p>
+	<span class="absolute right-5 bottom-0 text-zinc-400 group-hover:text-white/50"
+		>{publishedOn}</span
+	>
 </a>
